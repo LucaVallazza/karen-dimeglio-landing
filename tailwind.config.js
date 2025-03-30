43dd2fffff0781f08fbe746c23a1bf4de72737ed
LucaVallazza <Luvallazza@gmail.com>
2025-03-29 21:20:29 -0300

    feat: add Tailwind CSS configuration for custom color palette

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          navy: {
+            700: '#334155',
+            800: '#1e293b',
+            900: '#0f172a',
+          },
+          blue: {
+            50: '#eff6ff',
+            100: '#dbeafe',
+            200: '#bfdbfe',
+            600: '#2563eb',
+            700: '#1d4ed8',
+            800: '#1e40af',
+          },
+        }
+      }
+    }
+  }
